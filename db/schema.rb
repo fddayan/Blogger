00a7166ff3677672030a3395c769c8c844ca5fe6
Fernando Vitale <fernando.vitale@globant.com>
2012-05-21 16:08:05 -0300

    run migration for User Post Tag and TagsPost

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20120521183435) do
 
   create_table "posts", :force => true do |t|
     t.integer  "user_id"
     t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
-  
+
+  create_table "posts_tags", :force => true do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "label"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "lastname"
@@ -28,12 +40,6 @@ ActiveRecord::Schema.define(:version => 20120521183435) do
     t.string   "birthdate"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-
-  create_table "posts_tags", :force => true do |t|
-    t.integer "post_id"
-    t.integer "tag_id"
   end
 
 end
