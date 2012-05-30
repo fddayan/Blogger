3a1f87d07c76b17db92f36a464331e754aba973a
Adrian Rekofsky <adrian.rekofsky@globant.com>
2012-05-30 11:33:12 -0300

    a

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,11 @@ ActiveRecord::Schema.define(:version => 20120529133212) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
