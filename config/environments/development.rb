9641b47b3de47060c3aa31a871fbb482d47c78ea
mariajosealvarez <maria.alvarez@globant.com>
2012-05-30 11:32:08 -0300

    edit user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,11 +40,11 @@ Blogger::Application.configure do
 
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
   # change to false to prevent email from being sent during development
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default :charset => "utf-8"  
 
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
