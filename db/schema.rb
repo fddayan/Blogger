abd6e0fe8a14ced0c7519d1f1afaa61ff6c5070b
Fernando Vitale <fernando.vitale@globant.com>
2012-05-30 15:38:14 -0300

    image in posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20120530171917) do
-
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id"
