297423fdb6553ce9f538de47cc003068ff40c9ef
Fernando Vitale <fernando.vitale@globant.com>
2012-05-30 16:21:05 -0300

    image in posts4

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ActiveRecord::Base
 
   has_many :posts
   has_many :comments
-
+  has_and_belongs_to_many :users
+  
   validates_presence_of :lastname, :name
 	validates :email, :presence => true
  	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
