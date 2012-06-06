de6a128d9bb654c7a8d2767a719701be4f4e6d67
malvarez <malvarez@glb6626.(none)>
2012-06-06 12:24:49 -0300

    acts_as_taggablr

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,16 +66,7 @@ class PostsController < ApplicationController
     @user= User.find(params[:user_id])
     @post.user_id = (params[:user_id])
     tags = params[:tag].split(', ')
-#    @post.remove_all_tags
     tags.each do |t|
-=begin
-      tag = Tag.where(:label => t).first
-      if (tag == nil)
-        tag = Tag.new(:label => t)
-        tag.save
-      end
-      @post.tags << tag
-=end
       @post.tag_list.add(t)
     end
     respond_to do |format|
@@ -88,6 +79,7 @@ class PostsController < ApplicationController
       end
     end
   end
+
   # DELETE users/{id}/posts/1
   # DELETE users/{id}/posts/1.json
   def destroy
