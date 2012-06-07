383bca434a33e0950e15592a4588b8006018134e
Fernando Vitale <fernando.vitale@globant.com>
2012-06-07 17:08:19 -0300

    added some translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   	before_filter :set_locale
 
     def set_locale            
-      I18n.locale = params[:locale] || I18n.default_locale
+      I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
     end
-
+    
     def default_url_options(options={})
       { :locale => I18n.locale }
     end
