bad2ff49a255fb5bd243937f222a7fcb635cc035
mariajosealvarez <maria.alvarez@globant.com>
2012-06-07 13:08:31 -0300

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  	protect_from_forgery
+  	before_filter :set_locale
+
+    def set_locale      
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+
+    def default_url_options(options={})
+      { :locale => I18n.locale }
+    end
+
+    protect_from_forgery
     #check_authorization
   	check_authorization :unless => :devise_controller?
     
