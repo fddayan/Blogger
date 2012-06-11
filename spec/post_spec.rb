e2ec76e92d9e7359e55f0c4ce1db68d036fffcd6
Fernando Vitale <fernando.vitale@globant.com>
2012-06-11 15:52:28 -0300

    added rspec test2

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
 describe Post do
-  it "creates a new post" do
-    #@user = User.create :mail=>"fer@mail.com",:password=>"pass123", :name=>"fer",:lastname=>"vitale" 
 
-    post = Post.new :title=>"titulito de test",:content=>"contenido de test",:user_id=>123
-    post.title.should eq("titulito de test")
-    post.content.should eq("contenido de test")
-    post.user_id.should_not == nil
-
-    #assert_response 302
-    #assert_redirected_to "/users/#{@user.id}"
+  def do_create
+    #post :create, :menu_item=>{:name=>"value"}
+    post :create, :post=>{:title=>"titulito",:content=>"contenidito",:user_id=>123,:tag=>"taguito"}
   end
+
+  it "creates a new post" do
+    #MenuItem.should_receive(:new).with("name"=>"value").and_return(@menu_item)
+    Post.should_receive(:new)
+    do_create
+  end
+
 end
