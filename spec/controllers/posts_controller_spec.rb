807966557e74d04efc170dbaab84a8480f6524e4
fddayan <federico.dayan@gmail.com>
2012-06-13 14:21:40 -0300

    spec fixed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,21 +3,23 @@ require 'spec_helper'
 describe PostsController do
 
   include Devise::TestHelpers # to give your spec access to helpers
-  
-  before :all do 
-    @post = Post.create :content=>"xxxx", :title => "xxxx"
-  end
-  
-  it "creates a new post" do
-    post :create,:post=>@post.attributes
-  end
 
-  it "update a post" do
-    post :create,:id=>@post.id, :post=>@post.attributes
-  end
+  context "for REST actions " do   
+    before :all do 
+      @post = Post.create :content=>"xxxx", :title => "xxxx"
+    end
   
-  it "delete a post" do
-    post :destroy, :post=>@post        
+    it "creates a new post" do
+      post :create,:post=>@post.attributes
+    end
+
+    it "update a post" do
+      post :create,:id=>@post.id, :post=>@post.attributes
+    end
+  
+    it "delete a post" do
+      post :destroy, :post=>@post        
+    end
   end
 
 end
