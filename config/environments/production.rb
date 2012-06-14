b4504e82c1d808a6844ed9c1adbd7f30527373a9
Fernando Vitale <fernando.vitale@globant.com>
2012-06-14 16:05:35 -0300

    added pg for production8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Blogger::Application.configure do
 
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'cold-beach-7073.herokuapp.com' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   # change to false to prevent email from being sent during development
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
